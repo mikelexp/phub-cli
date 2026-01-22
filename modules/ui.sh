d868c3c22def6474a3b4e33fb7cdea455f306644
Miguel Scaramozzino <mikele@gmail.com>
2026-01-21 22:33:47 -0300

    more breadcrumbs + back to home & quit in every page except the results list pages

--- modules/ui.sh
+++ modules/ui.sh
@@ -35,30 +35,55 @@ EOF
 
 
 pre_play_menu() {
+    local video_title="$1"
+    local channel_name="$2"
+    local prompt="Action"
+
+    [ -n "$channel_name" ] && prompt="$prompt - $channel_name"
+    [ -n "$video_title" ] && prompt="$prompt - $video_title"
+
     printf "%s\n" \
         "watch|Watch video" \
         "download|Download video" \
         "browser|Open in browser" \
         "back|Back to results" \
-        | _fzf_menu "Action"
+        "home|Back to home" \
+        "quit|Quit" \
+        | _fzf_menu "$prompt"
 }
 
 quality_menu() {
+    local video_title="$1"
+    local channel_name="$2"
+    local prompt="Quality"
+
+    [ -n "$channel_name" ] && prompt="$prompt - $channel_name"
+    [ -n "$video_title" ] && prompt="$prompt - $video_title"
+
     printf "%s\n" \
         "1|Best quality (1080p+)" \
         "2|720p" \
         "3|480p" \
         "4|360p" \
         "back|Back" \
-        | _fzf_menu "Quality"
+        "home|Back to home" \
+        "quit|Quit" \
+        | _fzf_menu "$prompt"
 }
 
 post_download_menu() {
+    local video_title="$1"
+    local channel_name="$2"
+    local prompt="Download complete!"
+
+    [ -n "$channel_name" ] && prompt="$prompt - $channel_name"
+    [ -n "$video_title" ] && prompt="$prompt - $video_title"
+
     printf "%s\n" \
         "another|Download another quality" \
         "results|Back to results" \
         "home|Back to home" \
         "quit|Quit" \
-        | _fzf_menu "Download complete!"
+        | _fzf_menu "$prompt"
 }
 
